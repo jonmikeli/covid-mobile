d726f1eb964221a3fc5f1623464927ebe61aa183
Jon Mikel Inza <jonmikeli@hotmail.com>
2020-04-09 21:56:06 +0200

    script updates

--- deploy/IoTSolution/IoTSolution/AzureCLI/create-iotsolution.sh
+++ deploy/IoTSolution/IoTSolution/AzureCLI/create-iotsolution.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ./0-AzFullDeployment-NotNested.sh \
--s "ec91c862-9472-4bb7-9c61-64727c764b8b" \
+-s "[to be replaced]" \
 -r "IOT-COVID" \
 -t "../AzureFullDeployNotNestedARM.json" \
 -p "@../AzureFullDeploy.parameters-dev.json" \
